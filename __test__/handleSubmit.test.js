eeaf232edc6d14a5e0123126115bd423a09ec44d
baronmdw <58744855+baronmdw@users.noreply.github.com>
2022-12-02 18:18:51 +0100

    Mock Document Set up

--- __test__/handleSubmit.test.js
+++ __test__/handleSubmit.test.js
@@ -3,6 +3,12 @@ import { handleSubmit } from "../src/client/js/formHandler"
 describe("Handle Submit Tests", () => {
     test("Check if Request is sent", () => {
         // Test description
+        document.body.innerHMTL = 
+        '<div id="agreement"></div>'+
+        '<div id="confidence"></div>'+
+        '<div id="irony"></div>'+
+        '<div id="subjectivity"></div>';
+
         const input = "I feel happy";
         const ouptut = "positive";
         expect(handleSubmit).toBeDefined();
