a470e1e68601fa080c7d0bf07be65ab05615b115
baronmdw <58744855+baronmdw@users.noreply.github.com>
2022-12-02 13:11:57 +0100

    Response from meaningful api read

--- src/server/index.js
+++ src/server/index.js
@@ -10,7 +10,7 @@ const FormData = require('form-data');
 dotenv.config();
 
 const app = express()
-// app.use(bodyParser.urlencoded());
+
 app.use(bodyParser.json())
 
 const apiKey = process.env.API_KEY;
@@ -25,7 +25,6 @@ app.use(express.static('../../dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
     res.sendFile("index.html", {root: "../../dist"})
 })
 
@@ -37,38 +36,23 @@ app.listen(8081, function () {
 app.post('/test', function (req, res) {
     const inputData = req.body.inputText;
     requestBody["txt"] = inputData;
-    // const options = {
-    //     host: 'https://api.meaningcloud.com',
-    //     path: '/sentiment-2.1',
-    //     method: 'POST',
-    //     body: JSON.stringify(requestBody)
-    // };
     console.log(req.body.inputText);
     console.log(requestBody);
     const formularData = new FormData();
     formularData.append('key', apiKey);
-    // formularData.append('of', 'json');
+    formularData.append('of', 'json');
     formularData.append('lang', 'en');
     formularData.append('txt', inputData)
     console.log(formularData)
-    // request.open('POST', 'https://api.meaningcloud.com/sentiment-2.1');
-    // request.send(formularData);
-    // request.post({uri: 'https://api.meaningcloud.com/sentiment-2.1', formData: formularData}, function(error, response, body){
-    //                 console.log(body); 
-    //             }); 
+
     formularData.submit('https://api.meaningcloud.com/sentiment-2.1',function(err, res) {
-        console.log(res);
+        let body = "";
+        res.on('data', (dataPart) => {
+            body += dataPart;
+        })
+        res.on('end', () => {
+            console.log(body);
+        })
     });
-    // fetch('https://api.meaningcloud.com/sentiment-2.1', {
-    //     credentials: 'same-origin',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify(requestBody),
-    // })
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     console.log = res.body
-    // })
     res.send(mockAPIResponse)
 })
